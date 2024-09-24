8734ac2d06d12eb3864e1182c31e91ef54b5d292
RuderWest <91721895+matheobernabe@users.noreply.github.com>
2024-09-24 16:05:43 +0200

    branchez le mec

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const {Router} = require('express');
 const userController = require('../controllers/user_Controller');
 const router = new Router();
 
-router.post('/register', userController.post);
-
+router.post('/register', (req, res) =>{
+    console.log(req.body);
+    userController.post(req, res);
+});
 module.exports = router;
